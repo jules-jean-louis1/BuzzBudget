abdbc702ee4c1409364f09c7557973e31f3f57e0
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-11-09 15:30:52 +0100

    config

--- public/buzzbudget/src/components/modal/modalLogin.jsx
+++ public/buzzbudget/src/components/modal/modalLogin.jsx
@@ -3,7 +3,7 @@ import Login from "../form/login";
 
 export default function ModalLogin({ onClose }) {
     return (
-        <div className="modal">
+        <div className="modal_login">
           <div className="modal-content">
             <span className="close" onClick={onClose}>
               &times;
