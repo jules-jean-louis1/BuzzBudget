473e28fc36cbf7bb1f33675421040e2e7f3bb15f
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-11-09 11:45:19 +0100

    Refactored code to improve performance and readability.

--- api/Controller/AbstractClasses/AbstractContoller.php
+++ api/Controller/AbstractClasses/AbstractContoller.php
@@ -4,5 +4,28 @@ namespace App\Controller\AbstractClasses;
 
 class AbstractContoller
 {
+    protected function verifyField($field): mixed
+    {
+        if (isset($_POST[$field]) && !empty(trim($_POST[$field]))) {
+            return $_POST[$field];
+        } else {
+            return false;
+        }
+    }
+    protected function ValidFieldForm(string $field): string
+    {
+        $field = trim($field);
+        $field = htmlspecialchars($field);
 
+        return $field;
+    }
+    protected function VerifyPassword(string $password): bool
+    {
+        // 8 caractères minimum, 3 lettres minuscules minimum, 2 lettres majuscules minimum, 2 chiffres minimum, 1 caractère spécial minimum
+        if (preg_match(pattern: "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/", subject: $password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- api/Model/AuthModel.php
+++ api/Model/AuthModel.php
@@ -1,8 +1,27 @@
 <?php
 
 namespace App\Model;
-
-class AuthModel
+use App\Model\AbstractDatabase;
+class AuthModel extends AbstractDatabase
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function verifyUser(string $email): bool
+    {
+        $req = $this->getBdd()->prepare('SELECT id_users FROM users WHERE email = :email');
+        $req->bindValue(':email', $email, \PDO::PARAM_STR);
+        $req->execute();
+        $user = $req->fetch();
+        if ($user) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function register()
+    {
 
+    }
 }
