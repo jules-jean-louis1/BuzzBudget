1564ee037aa320ac8bfef1e102f56123cd545722
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-11-09 12:05:47 +0100

    Add semi-working code for page navigation

--- public/buzzbudget/src/App.jsx
+++ public/buzzbudget/src/App.jsx
@@ -9,3 +9,34 @@ function App() {
 }
 
 export default App
+
+// semi-working code for page navigation
+
+/* import React, { useState } from 'react';
+import LandingPage from './components/LandingPage';
+import BudgetPage from './components/BudgetPage';
+
+function App() {
+  const [page, setPage] = useState('landing');
+
+  const renderPage = () => {
+    switch(page) {
+      case 'landing':
+        return <LandingPage />;
+      case 'budget':
+        return <BudgetPage />;
+      default:
+        return <LandingPage />;
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={() => setPage('landing')}>Go to Landing Page</button>
+      <button onClick={() => setPage('budget')}>Go to Budget Page</button>
+      {renderPage()}
+    </div>
+  );
+}
+
+export default App; */
