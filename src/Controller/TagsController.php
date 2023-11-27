05eb215de82583e30aee46f75869ae1bc5150b79
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-11-27 12:09:56 +0100

    Refactor TagsController and TagsModel classes

--- src/Controller/TagsController.php
+++ src/Controller/TagsController.php
@@ -1,8 +1,38 @@
 <?php
 
 namespace App\Controller;
-
-class TagsController
+use App\Model\TagsModel;
+class TagsController extends AbstractClasses\AbstractContoller
 {
+    public function getTags(): void
+    {
+        $tags = new TagsModel();
+        $user = $_SESSION['user'];
+        $id = $user->getId();
+        $tags = $tags->getTags($id);
+        echo json_encode($tags);
+    }
+    public function add(): void
+    {
+        $input_tags = $this->verifyField('tags');
+        $user = $_SESSION['user'];
+        $id = $user->getId();
+        $tags = new TagsModel();
 
+        $errors = [];
+
+        if (!$input_tags) {
+            $errors['tags'] = 'Entrer le nom du tag';
+        } elseif (strlen($input_tags) < 2 || strlen($input_tags) > 50) {
+            $errors['tags'] = 'Le nom du tag doit contenir entre 2 et 50 caractères';
+        }
+
+        if (empty($errors)) {
+            $tags->addTags($input_tags, $id);
+            $errors['success'] = 'Le tag a bien été ajouté';
+            echo json_encode($errors);
+        } else {
+            echo json_encode($errors);
+        }
+    }
 }
--- src/Model/TagsModel.php
+++ src/Model/TagsModel.php
@@ -2,7 +2,21 @@
 
 namespace App\Model;
 
-class TagsModel
+class TagsModel extends AbstractDatabase
 {
 
+    public function getTags(int $id): array
+    {
+        $req = $this->getBdd()->prepare('SELECT * FROM tags WHERE users_id = :id');
+        $req->bindParam(':id', $id, \PDO::PARAM_INT);
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function addTags(string $tags, int $id): void
+    {
+        $req = $this->getBdd()->prepare('INSERT INTO tags (name_tags, users_id, created_at) VALUES (:name, :id, NOW())');
+        $req->bindParam(':name', $tags, \PDO::PARAM_STR);
+        $req->bindParam(':id', $id, \PDO::PARAM_INT);
+        $req->execute();
+    }
 }
