bb8a90214457a7c44ed65f3c4cdd441a0b0667c1
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-11-27 08:57:11 +0100

    Add GET request with credentials to fetch categories

--- public/buzzbudget/src/components/hook/useCategories.jsx
+++ public/buzzbudget/src/components/hook/useCategories.jsx
@@ -8,7 +8,11 @@ function useCategories() {
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
-        "http://localhost:80/buzzbudget/src/categories/get/"
+        "http://localhost:80/buzzbudget/src/categories/get/",
+        {
+          method: "GET",
+          credentials: "include",
+        }
       );
       const result = await response.json();
       console.log(result);
