2e7c3cdd09cc884afba94e69ee78f525f16de253
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-01-03 14:27:17 +0100

    Add build:css script and import Tailwind CSS styles

--- public/buzzbudget/tailwind.config.js
+++ public/buzzbudget/tailwind.config.js
@@ -1,16 +1,14 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-custom': 'linear-gradient(20deg, #FF2E00, #FD9D58)',
+        "gradient-custom": "linear-gradient(20deg, #FF2E00, #FD9D58)",
       },
     },
   },
   plugins: [],
-}
-
+};
